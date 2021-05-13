3aa1e096d34d0bfaccd99902ecc3b6b0bf144bb7
Akshay Padghan <32366204+padghanakshay@users.noreply.github.com>
2021-05-13 16:10:40 +0530

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VaccineSlotter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Alert when vacchine available")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Open Source Jan Hit Mai Jari")]
 [assembly: AssemblyProduct("VaccineSlotter")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("NULL")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
